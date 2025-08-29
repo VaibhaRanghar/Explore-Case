f2d6b254729a1d6c35c892a4f295786baf18f6f2
Pengyuwin-Sama <vaibhavranghar150@gmail.com>
2025-08-29 12:51:38 +0530

    fixed contact form

--- src/app/api/sendEmail/route.ts
+++ src/app/api/sendEmail/route.ts
@@ -2,35 +2,30 @@ import nodemailer from "nodemailer";
 
 export async function POST(request: Request) {
   try {
-    // Parse the request body to get form data
     const { name, email, phone, query } = await request.json();
 
-    // Create a transporter object with SMTP configuration
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: parseInt(process.env.SMTP_PORT || "587"),
-      secure: false, // Use TLS (secure: true for port 465)
       auth: {
         user: process.env.EMAIL,
         pass: process.env.PASSWORD,
       },
     });
-    console.log(process.env.EMAIL, process.env.PASSWORD);
-    // Define email options
+
     const mailOptions = {
-      from: process.env.EMAIL, // Sender email address
-      to: process.env.SITE_MAIL_RECEIVER, // Receiver email address
+      from: process.env.EMAIL,
+      to: process.env.SITE_MAIL_RECEIVER,
       subject: `New Contact Form Submission from ${name}`,
       text: `
         Name: ${name}
         Email: ${email}
         Phone: ${phone}
         Query: ${query}
-      `, // Plain text body
-      replyTo: email, // Reply-to email address
+      `,
+      replyTo: email,
     };
 
-    // Send the email
     const info = await transporter.sendMail(mailOptions);
 
     return new Response(

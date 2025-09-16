3060b2429bb448ec3bcf00c3d562d784c1decdd0
Pengyuwin-Sama <vaibhavranghar150@gmail.com>
2025-09-16 16:12:32 +0530

    relative import used for footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/NavBar";
-
 import WhatsAppButton from "@/components/WhatsappBtn";
-import Footer from "@/components/Footer";
+import Footer from "../components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",

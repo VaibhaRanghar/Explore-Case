4c36f369e16945f407c16a57d54e4a79319c488d
Pengyuwin-Sama <vaibhavranghar150@gmail.com>
2025-09-16 16:21:05 +0530

    footer import error fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/NavBar";
 import WhatsAppButton from "@/components/WhatsappBtn";
-import Footer from "../components/Footer";
+import Footer from "@/components/footer";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
